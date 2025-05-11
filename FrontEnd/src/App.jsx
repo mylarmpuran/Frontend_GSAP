82164e8d2dab1af122d52f7c27d9ed68fe3b31b5
puran kumar <mylarmpuran@gmail.com>
2025-05-11 18:16:26 +0530

    "navbar"

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from './components/Navbar'
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='font-bold text-amber-300'>Hello world</h1>
+       <Navbar/>
     </>
   )
 }
