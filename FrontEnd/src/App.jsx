624bad036ebd960ca5e5c1219a4e15100755c3a2
puran kumar <mylarmpuran@gmail.com>
2025-05-11 20:03:27 +0530

    sections

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Navbar from './components/Navbar'
+import HeroSection from './components/HeroSection'
+import FeatureSection from './components/FeatureSection'
 
 
 
@@ -11,6 +13,8 @@ function App() {
   return (
     <>
        <Navbar/>
+       <HeroSection/>
+       <FeatureSection/>
     </>
   )
 }
---
+++ FrontEnd/src/components/Workflow.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { checkCircle2 } from "lucide-react";
+import codeImg from "../assets/code.jpg"
+
+function Workflow() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Workflow
