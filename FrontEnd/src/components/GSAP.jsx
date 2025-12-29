d571c3ad97d776431a7f07cf8b62e49f8e4f7e24
puran kumar <mylarmpuran@gmail.com>
2025-12-29 23:45:46 +0530

    change

--- FrontEnd/src/components/GSAP.jsx
+++ FrontEnd/src/components/GSAP.jsx
@@ -1,30 +1,44 @@
 import { useRef } from "react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
+import { SplitText } from "gsap/all";
 
-// gsap.registerPlugin(useGSAP);
+gsap.registerPlugin(SplitText);
 
 function GSAP()  {
-    const green = useRef();
-    const purpule = useRef();
-    const blue = useRef();
+    // const green = useRef();
+    // const purpule = useRef();
+    // const blue = useRef();
     useGSAP(() => {
             // Refs allow you to access DOM nodes
-            console.log(green)
+            // console.log(green)
 
             // then we can animate them like so..
-           gsap.to(".green", {rotation: 360, x: 100, duration: 1});
-           gsap.from(".purple", {rotation: -360, x: -100, duration: 1});
-           gsap.fromTo(".blue", {x: -100},{rotation: 360, x: 100, duration: 1});
+           gsap.to("#green", {rotation: 360, x: 100, duration: 1});
+           gsap.from("#purple", {rotation: -360, x: -100, duration: 5});
+           gsap.fromTo("#blue", {x: -100},{rotation: 360, x: 100, duration: 10});
+
+           let split = SplitText.create(".split", {type: "words, chars"});
+
+           gsap.from(split.chars, {
+            duration:1,
+            y:100,
+            autoAlpha:0,
+            stagger:0.05
+           })
 
         });
 
         return (
                     <div className="flex flex-col justify-center w-96">
                         
-                        <div className="green h-80 w-35 border-2 border-red-500 mt-10" ref={green}>Komal</div>
-                        <div className="purple h-80 w-35 border-2 border-red-500 mt-10" ref={purpule}>Komal</div>
-                        <div className="blue h-80 w-35 border-2 border-red-500 mt-10" ref={blue}>Komal</div>
+                        <div id = "green" className="h-80 w-35 border-2 mt-10 bg-red-600" >Komal</div>
+                        <div id = "purple" className="h-80 w-35 border-2 mt-10 bg-green-600" >Aadya</div>
+                        <div id="blue" className="h-80 w-35 border-2 mt-10 bg-blue-600" >Aagastya</div>
+                        <div className="split">With autoSplitenabled, you should always create your animations in the onSplit() callback so that
+                            if it re-splits later, the resulting animations affect the freshly-created line/word/character elements instead of the ones from the
+                            previous split.
+                        </div>
                         
                     </div>
             );
